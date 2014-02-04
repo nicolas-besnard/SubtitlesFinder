42bb41c1807c01d1e2e914562fde0ae65c9e6576
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-02-04 20:13:29 +0100

    Init Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubtitlesFinder::Application.config.secret_token = '9cd27fb009590d4cec37145a719b2794c38ee7b1c31e6962d56a5494f5b6ef095d53c32d73ff9a0f7d6158f69b26d7158ec1de649b50cabb6ea0e6f056f3bfec'
